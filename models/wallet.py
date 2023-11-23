f3597d5178f5eafafef4aa2ec6ce17890fb4cc48
Vyacheslav <polyerter@gmail.com>
2023-11-23 17:54:42 +0700

    update

--- models/wallet.py
+++ models/wallet.py
@@ -2,13 +2,23 @@
 from config.database import Base
 
 
-from sqlalchemy import Column, Integer, String, Float
-# from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Wallet(Base):
-    __tablename__ = "wallets"
+    __tablename__ = 'wallets'
 
     id = Column(Integer, primary_key=True, index=True)
-    currency = Column(String)
     amount = Column(Float)
+    currency_id = Column(Integer, ForeignKey("currencies.id"))
+    currency = relationship('Currency')
+
+
+class Currency(Base):
+    __tablename__ = 'currencies'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    # wallets = relationship("Wallet", back_populates="wallets")
+
