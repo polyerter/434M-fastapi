82c63143cc8f209915b69636ba560e92b92c57cc
Vyacheslav <polyerter@gmail.com>
2023-11-23 17:55:02 +0700

    update

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from typing import Union
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from models.wallet import Wallet
+from models.wallet import Wallet, Currency
 
-from schemas.wallet_schemas import Response, CreateRequestWallet
+from schemas.wallet_schemas import Response, CreateRequestWallet, CreateRequestCurrency
 # from models import Body
-from config.database import engine, get_db
+from config.database import Base, engine, get_db
 from pydantic import BaseModel
 
-Wallet.metadata.create_all(bind=engine)
+# Wallet.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
@@ -18,23 +19,49 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    external_wallet = {
-        'currency': 'rub',
-        'amount': 156.10
-    }
+    # external_wallet = {
+    #     'currency': 'rub',
+    #     'amount': 156.10
+    # }
     
-    wallet = Wallet(**external_wallet)
+    # wallet = Wallet(**external_wallet)
 
-    return wallet
+    return {}
 
+
+@app.post("/currencies")
+async def currencies(currency: CreateRequestCurrency, db: Session = Depends(get_db)):
+    rec = Currency(**currency.dict())
+     
+    db.add(rec)
+    db.commit()
+    db.refresh(rec)
+        
+    return Response(
+        status=200, 
+        message="Ok",
+        data=rec
+    )
+
+    return {}
 
 @app.get("/wallets")
-def wallets(q: Union[str, None] = None):
-    return {"q": q}
-
+def wallets(db: Session = Depends(get_db)):
+    wallets = db.query(Wallet).all()
+    
+    return wallets
 
 @app.post("/wallets")
 async def wallets(wallet: CreateRequestWallet, db: Session = Depends(get_db)):
+    currency = db.query(Currency).get(wallet.currency_id)
+
+    if not currency:
+        return Response(
+            status=404,
+            message="Not found",
+            data={}
+        )
+
     w = Wallet(**wallet.dict())
   
     db.add(w)
